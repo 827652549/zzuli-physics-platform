ce5af08d2687007cf67ac863e21caee6103b1223
junking <827652549@qq.com>
2020-02-28 22:00:36 +0800

    更改了姓名

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -126,7 +126,7 @@ class Header extends Component {
                             <NavItem className={'left'}>
                                 <Link to={'/'} className="aLink navigation-link">
                                     <span className="iconfont">&#xe6f0;&nbsp;</span>
-                                    苏一恒
+                                    一恒
                                 </Link>
                             </NavItem>
                         </Col>
